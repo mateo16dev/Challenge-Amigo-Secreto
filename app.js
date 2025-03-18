fd6dbb4e19b6c75cc1ede0be6ba3d96e014a57b8
mateo16dev <mateo.coca@outlook.com>
2025-03-18 12:15:52 -0300

    Creacion nde funcion agregarAmigos ()

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos [];
+let amigos = [];
 
-function asignarAmigos(){
-    let ingresoAmigo = document.getElementById('amigo');
+function agregarAmigo() {
+    let ingresoAmigo = document.getElementById("amigo");
     let nombre = ingresoAmigo.value.trim();
 
-    if (nombre === ""){
+    if (nombre === "") {
         alert("Por favor, ingrese un nombre.");
         return;
     }
 
     amigos.push(nombre);
 
+    actualizarLista();
+
     ingresoAmigo.value = "";
 
-    console.log(amigos);
 }
-
